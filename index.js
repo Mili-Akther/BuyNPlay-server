b8759674e3339886e416ba8f44e43b8cba6f950c
Mili Akther <miliakthermilu@gmail.com>
2025-05-15 23:51:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res)=>{
+      res.send("hello");
+})
+
+app.listen(port, ()=>{
+      console.log("Server is Running...");
+});
